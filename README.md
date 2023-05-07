d209d77e10c2acbefcd04af02710c2c6a5f73afc
Angelina Tingaeva <angelinatingaeva@yandex.ru>
2023-05-08 00:32:00 +0500

    startprogect

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
 # praktikum_new_diplom
+
+# Для примера
+# yamdb_final
+
+![status workflow](https://github.com/Angelina91/yamdb_final/actions/workflows/yamdb_workflow.yml/badge.svg)
+
+# Краткое описание проекта
+YaMDb_final - программный интерфейс для социальной сети.
+Он позволяет неравнодушным творческим людям обсудить заинтересовавшие их произведения: оставить отзывы к книгам, фильмам и т.д. и прокоментировать уже существующие отзывы
+
+## Документация
+
+[redoc](http://84.201.158.17/redoc/)
+## :dash: Установка и запуск проекта
+
+### :dancers: Клонирование репозитория
+
+git clone [SSH](git@github.com:Angelina91/yamdb_final.git)
+
+### :whale: Запуск всех контейнеров
+
+Из директории infra/ выполнить команду
+
+```bash
+docker-compose up -d --build
+```
+
+### :feet: Выполнить миграции
+
+```bash
+docker-compose exec web python manage.py migrate
+```
+
+### :bowtie: Создать суперпользователя
+
+```bash
+docker-compose exec web python manage.py createsuperuser
+```
+
+### :crystal_ball: Собрать статику
+
+```bash
+docker-compose exec web python manage.py collectstatic --no-input
+```
+
+### :love_letter: Заполнить базу данных
+
+```bash
+docker-compose exec web python manage.py loaddata ../infra/fixtures.json
+```
+
+## :dizzy: Стек
+
+_Python,
+Rest Api,
+Docker,
+PostgreSQL,
+Redoc,
+Django,
+djangorestframework_
+
+## Путь до удаленного сервера
+
+```bash
+ssh angelina91@84.201.158.17
+```
+
+## Автор
+
+[Angelina91](https://github.com/Angelina91)
---
+++ backend/foodgram_progect/foodgram_progect/settings.py
@@ -0,0 +1,125 @@
+import os
+
+from datetime import timedelta
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure-=j%#=pc5l3l5b-s8vf!)$tl+_z#&4pr&iv6fuhxg04esnw*_9i')
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+#SECRET_KEY = 'django-insecure-=j%#=pc5l3l5b-s8vf!)$tl+_z#&4pr&iv6fuhxg04esnw*_9i'
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'foodgram_progect.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'foodgram_progect.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
