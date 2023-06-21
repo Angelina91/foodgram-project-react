666da33dd0b2339f2ad503d7456baa6afa6da6ea
Angelina Tingaeva <angelinatingaeva@yandex.ru>
2023-06-21 16:52:26 +0500

    filter

---
+++ backend/foodgram_progect/api/filters.py
@@ -0,0 +1,47 @@
+from rest_framework import generics
+from django_filters import rest_framework as filters
+from posts.models import Ingredient, Tag, Recipe
+
+
+class IngredientList(generics.ListAPIView):
+    queryset = Ingredient.objects.all()
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = ('name',)
+
+
+# Equivalent FilterSet:
+class IngredientFilter(filters.FilterSet):
+    name = filters.CharFilter(lookup_expr='startswith')
+
+    class Meta:
+        model = Ingredient
+        fields = ('name',)
+
+
+class RecipeFilter(filters.FilterSet):
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all(),
+    )
+
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart'
+    )
+
+    class Meta:
+        model = Recipe
+        fields = ('tags', 'author', 'is_in_shopping_cart')
+
+    def filter_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if value and not user.is_anonymous:
+            return queryset.filter(in_favorite__user=user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value and not user.is_anonymous:
+            return queryset.filter(shopping_list__user=user)
+        return queryset
