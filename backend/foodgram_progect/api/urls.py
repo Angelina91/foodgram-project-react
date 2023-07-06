8f408ee06f714878aaeca125e127c9c9287ce579
Angelina Tingaeva <angelinatingaeva@yandex.ru>
2023-07-06 19:32:45 +0500

    mixins+isort

--- backend/foodgram_progect/api/urls.py
+++ backend/foodgram_progect/api/urls.py
@@ -2,8 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (CustomUserViewSet, IngredientViewSet,
-                    RecipeCreateListRetrieveViewSet,
-                    TagViewSet)
+                    RecipeCreateListRetrieveViewSet, TagViewSet)
 
 app_name = 'api'
 
